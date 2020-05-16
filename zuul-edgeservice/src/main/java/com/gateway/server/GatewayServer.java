177e66da343b8ea8c93a6b21083a0e83c6c7f5eb
Rodrigo Prado <rodrigo.prado@bee-eng.pt>
2020-05-16 22:53:25 +0100

    Implement zuul security

---
+++ zuul-edgeservice/src/main/java/com/gateway/server/GatewayServer.java
@@ -0,0 +1,17 @@
+
+package com.gateway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableZuulProxy
+@ComponentScan("com.gateway")
+public class GatewayServer {
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayServer.class, args);
+  }
+}
+
